04d462d5f2f7bd97e6a3f6abe5a0fef49dce9aae
Rodrigo Eustáquio Duque <32581219+rodrigoeduque@users.noreply.github.com>
2021-03-18 17:04:22 -0300

    Projeto criado durante o curso - implementado botão limpar

---
+++ app/src/main/java/com/example/calculodenotas/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.calculodenotas
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btVerificar = buttonVerificar
+        val resultado = textViewResultado
+        val btLimparFormularios = buttonLimpar
+
+        btVerificar.setOnClickListener {
+            val nota1 = Integer.parseInt(editTextNota1.text.toString())
+            val nota2 = Integer.parseInt(editTextNota2.text.toString())
+            val media = (nota1 + nota2)/2
+            val faltas = Integer.parseInt(editTextFaltas.text.toString())
+
+
+
+            if (media >= 6 && faltas <= 10){
+                resultado.text = "Aluno foi Aprovado \n Nota Final: $media \n Faltas: $faltas"
+                resultado.setTextColor(Color.GREEN)
+            }
+            else {
+                resultado.text = "Aluno Reprovado \n Nota Final: $media \n Faltas: $faltas"
+                resultado.setTextColor(Color.RED)
+            }
+        }
+
+        btLimparFormularios.setOnClickListener {
+            val nota1 = editTextNota1.text.clear()
+            val nota2 = editTextNota2.text.clear()
+            val faltas = editTextFaltas.text.clear()
+            resultado.text = ""
+        }
+
+
+    }
+}
